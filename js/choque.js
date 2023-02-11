bdb0545cc2f5e259919468fc9560e75b09e66a99
Amado Melguizo <56071729+Amado-Melguizo@users.noreply.github.com>
2023-02-12 00:48:26 +0100

    Update choque.js

--- js/choque.js
+++ js/choque.js
@@ -1,20 +1,38 @@
 
 /* Código que maneja los eventos de los choques de los divs */
 
-
 // Prueba si quieres con un contador de fallos (no esta pulido)
-//var count = 0;
-//let countdiv = document.getElementById("count");
-
+var count = 0;
+let countdiv = document.getElementById("count");
+countdiv.innerHTML = `Score: ${count}`;
 
 // identificamos las variables
 const bb8 = document.getElementById("bb8");
+const terreno = document.getElementById("terreno");
 const misil = document.getElementsByClassName("misil");
 
+setInterval(() => {
+  for (const misilDiv of misil) {
+    const misilDivRect = misilDiv.getBoundingClientRect();
+    const terrenoRect = terreno.getBoundingClientRect();
+
+    if (misilDivRect.bottom > terrenoRect.top) {
+      // opcion contador
+
+      console.log("choca");
+      count++;
+      countdiv.innerHTML = `Score: ${count}`;
+
+      if (count === 100) {
+        alert("¡Has ganado!");
+        location.reload();
+      }
+    }
+  }
+}, 100);
 
 // Evento del raton
 addEventListener("mousemove", () => {
-
   /* Funcion  --> getBoundingClientRect() 
      Funcion que sirve 
      para identificacion de 
@@ -33,17 +51,6 @@ addEventListener("mousemove", () => {
       bb8Rect.top < misilDivRect.bottom &&
       bb8Rect.bottom > misilDivRect.top
     ) {
-
-      // opcion contador 
-
-      // count++;
-      // countdiv.innerHTML = `? Fallos ¿ ${count}`;
-      // if (count === 500) {
-      //   alert("¡Has perdido!");
-      //   location.reload();
-      // }
-
-
       // opcion Normal
       alert("¡Has perdido!");
       location.reload();
